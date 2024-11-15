2df5aaf3f62ef2efd97aca09419bd854a8de7f90
Aarav Shah <codingaarav1@gmail.com>
2024-11-15 14:33:24 -0800

    hi

--- Main.java
+++ Main.java
@@ -4,37 +4,11 @@ import javax.sound.sampled.Clip;
 
 
 public class Main {
-    private enum ChatbotState {
-        HELP, ERROR, MAINMENU, MUSICPLAYER, QUIT
-    }
-
-    private enum HelpState {
-        MAINMENU, MUSICPLAYER
-    }
-    private enum ErrorState {
-        MAINMENU, MUSICPLAYER
-    }
-    private static ChatbotState state;
-    private static HelpState helpState;
-    private static ErrorState errorState;
+   
     static boolean validResponse = true;
     public static void main(String[] args) {
-        state = ChatbotState.MAINMENU;
-        while (state != ChatbotState.QUIT) {
-            botOutput();
-        }
-    }
-
-    static void botOutput() {
-        switch (state) {
-            case ChatbotState.QUIT:
-                System.out.println("Have a great day!");
-                break;
-            case ChatbotState.MAINMENU:
-                System.out.println("Welcome to the UNTHEMED Chatbot");
-                System.out.println("How can I help you today?");
-                getResponse();
-                break;
+        while (true) {
+          // loop
         }
     }
 
@@ -52,9 +26,4 @@ public class Main {
       }
     }).start();
       }
-
-
-    static void getResponse() { // take user input
-        
-    }
 }
