6356aed33db74ee27d9d9e708cbf3f3b57a67386
ad4133 <ad4133@pleasantonusd.net>
2024-11-15 14:36:25 -0800

    Create MusicPlayer.java

---
+++ MusicPlayer.java
@@ -0,0 +1,25 @@
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
+
+public class MusicPlayer {
+    public static void main(String[] args) {
+      playSound("test.wav");
+    }
+
+    public static synchronized void playSound(final String url) {
+        new Thread(new Runnable() {
+      public void run() {
+        try {
+          Clip clip = AudioSystem.getClip();
+            AudioInputStream inputStream = AudioSystem.getAudioInputStream(Main.class.getResourceAsStream(url));
+          clip.open(inputStream);
+          clip.start(); 
+        } catch (Exception e) {
+          System.err.println(e.getMessage());
+        }
+      }
+    }).start();
+      }
+}
