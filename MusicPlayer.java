5dc33ac63d1710e2b735154b50db37d20f57f85f
Aarav Shah <codingaarav1@gmail.com>
2024-11-21 14:08:55 -0800

    finish music player

--- MusicPlayer.java
+++ MusicPlayer.java
@@ -6,21 +6,24 @@ import java.io.File;
 import javax.swing.JOptionPane;
 
 public class MusicPlayer {
+    Clip clip;
     public static void main(String[] args) {
-      playSound("bird_caw2.wav");
+      new MusicPlayer().playSound("bird_caw2.wav");
       // JOptionPane.showMessageDialog(null, "press ok to stop playing");
     }
 
-    public static void playSound(String url) {
+    public void stopSound() {
+      clip.stop();
+    }
+
+    public void playSound(String url) {
       try {
         File musicPath = new File(url);
         if(musicPath.exists()) {
           AudioInputStream audioInput = AudioSystem.getAudioInputStream(musicPath);
-          Clip clip = AudioSystem.getClip();
+          clip = AudioSystem.getClip();
           clip.open(audioInput);
           clip.start();
-          JOptionPane.showMessageDialog(null, "press ok to stop playing");
-          clip.stop();
         } else {
           System.out.println("oof");
         }
